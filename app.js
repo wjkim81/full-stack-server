bcda0c9b42547e63abf1614e335d1fd1753c8206
wjkim81 <onejean81@gmail.com>
2018-04-05 18:40:34 +0900

    Passport JWT

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var session = require('express-session');
 var FileStorage =require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
-
+var config = require('./config');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,7 +21,7 @@ mongoose.Promise = require('bluebird');
 const Dishes = require('./models/dishes');
 
 // Connection URL
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -41,6 +41,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
+/*
 app.use(session({
   name: 'session-id',
   secret: '12345-67890-09876-54321',
@@ -48,27 +49,16 @@ app.use(session({
   resave: false,
   store: new FileStorage()
 }));
+*/
 
 app.use(passport.initialize());
+
+/*
 app.use(passport.session());
+*/
 
 app.use('/', index);
 app.use('/users', users);
-
-function auth (req, res, next) {
-  console.log(req.user);
-
-  if(!req.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
-  }
-  else {
-    next();
-  }
-};
-
-app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
